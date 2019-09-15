a4988826b7515d49633540d8e9206a9422256970
pugongyingh <pgyhh@sina.cn>
2019-09-15 15:57:44 +0800

    Update fetch-joke.js

--- src/functions/fetch-joke/fetch-joke.js
+++ src/functions/fetch-joke/fetch-joke.js
@@ -2,7 +2,7 @@
 const fetch = require("node-fetch");
 exports.handler = async function(event, context) {
   try {
-    const response = await fetch("https://icanhazdadjoke.com", {
+    const response = await fetch("https://bing.com", {
       headers: { Accept: "application/json" }
     });
     if (!response.ok) {
