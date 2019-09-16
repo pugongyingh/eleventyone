3d6b42222fcf1b22ade6c04aeacca3a339a6d8ea
pugongyingh <pgyhh@sina.cn>
2019-09-16 19:09:14 +0800

    Update fetch-joke.js

--- src/functions/fetch-joke/fetch-joke.js
+++ src/functions/fetch-joke/fetch-joke.js
@@ -9,12 +9,9 @@ exports.handler = async function(event, context) {
       // NOT res.status >= 200 && res.status < 300
       return { statusCode: response.status, body: response.statusText };
     }
-    const data = await response.json();
+  
 
-    return {
-      statusCode: 200,
-      body:  { msg: response.statusText }
-    };
+   return { statusCode: response.status, body: response.statusText };
   } catch (err) {
     console.log(err); // output to netlify function log
     return {
