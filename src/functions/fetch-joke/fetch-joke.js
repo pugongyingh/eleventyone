1c256215b6dde5879bb4cc87091a7a24faf510f3
pugongyingh <pgyhh@sina.cn>
2019-09-16 19:12:48 +0800

    Update fetch-joke.js

--- src/functions/fetch-joke/fetch-joke.js
+++ src/functions/fetch-joke/fetch-joke.js
@@ -1,17 +1,21 @@
+
 /* eslint-disable */
 const fetch = require("node-fetch");
 exports.handler = async function(event, context) {
   try {
-    const response = await fetch("https://spreadsheets.google.com/feeds/list/1CfI6XGm9OjjNKGr3kXRSKVLui_gkHZdadoOPIiNgE9s/od6/public/values?alt=json", {
+    const response = await fetch("https://icanhazdadjoke.com", {
       headers: { Accept: "application/json" }
     });
     if (!response.ok) {
       // NOT res.status >= 200 && res.status < 300
       return { statusCode: response.status, body: response.statusText };
     }
-  
+    const data = await response.json();
 
-   return { statusCode: response.status, body: response.statusText };
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ msg: data.joke })
+    };
   } catch (err) {
     console.log(err); // output to netlify function log
     return {
