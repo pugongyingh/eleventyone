37a6e02ab049856a7a4b3fcf122552e3e9a03339
pugongyingh <pgyhh@sina.cn>
2019-09-16 21:40:56 +0800

    Update hello.js

--- src/functions/hello/hello.js
+++ src/functions/hello/hello.js
@@ -1,7 +1,50 @@
-exports.handler = async (event, context) => {
-  try {
-    return { statusCode: 200, body: `Hello from999 a serverless function!` };
-  } catch (err) {
-    return { statusCode: 500, body: err.toString() };
-  }
-};
+
+
+
+const axios  = require('axios');
+//const seed   = require('../../../utils/save-seed.js');
+
+
+// Once a googel sheet is "published to the web" we can access its JSON
+// via a URL of this form. We just need to pass in the ID of the sheet
+// which we can find in the URL of the document.
+const sheetID = "1CfI6XGm9OjjNKGr3kXRSKVLui_gkHZdadoOPIiNgE9s";
+const googleSheetUrl = `https://docs.google.com/spreadsheets/d/1_Xj3V6ovLEnK6u73PofcrJlG-XsgxBaQGK_wyUPXpYo/edit?usp=sharing`;
+
+module.exports = () => {
+  return new Promise((resolve, reject) => {
+
+    console.log(`Requesting data from ${googleSheetUrl}`);
+
+    axios.get(googleSheetUrl)
+      .then(response => {
+
+        // massage the data from the Google Sheets API into
+        // a shape that will more convenient for us in our SSG.
+        var data = {
+          "East": [],
+          "West": []
+        };
+        response.data.feed.entry.forEach(item => {
+          data[item.gsx$conference.$t].push({
+            "name": item.gsx$name.$t,
+            "team": item.gsx$team.$t
+          })
+        });
+
+        // stash the data locally for developing without
+        // needing to hit the API each time.
+        //seed(JSON.stringify(data), `${__dirname}/../dev/sheet.json`);
+
+        // resolve the promise and return the data
+        resolve(data);
+
+      })
+
+      // uh-oh. Handle any errrors we might encounter
+      .catch(error => {
+        console.log('Error :', error);
+        reject(error);
+      });
+  })
+}
