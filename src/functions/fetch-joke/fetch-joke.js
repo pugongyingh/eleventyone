b24a767742aafd7969214fb3ea358efbf8f860bd
pugongyingh <pgyhh@sina.cn>
2019-09-17 09:40:38 +0800

    Update fetch-joke.js

--- src/functions/fetch-joke/fetch-joke.js
+++ src/functions/fetch-joke/fetch-joke.js
@@ -14,7 +14,7 @@ exports.handler = async function(event, context) {
 
     return {
       statusCode: 200,
-      body: response.json()
+      body: data
     };
   } catch (err) {
     console.log(err); // output to netlify function log
