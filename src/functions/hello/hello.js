e956fe6bc34dcff72d7cb6741be0f847d6995903
pugongyingh <pgyhh@sina.cn>
2019-09-19 22:34:17 +0800

    Update hello.js

--- src/functions/hello/hello.js
+++ src/functions/hello/hello.js
@@ -1,28 +1,6 @@
+require('env').config();
 
-import axios from 'axios'
-
-export async function handler() {
-  const endpoint = 'https://api.instagram.com/v1/users/self/media/recent'
-  const token = '5414559434.a5e0d4b.f4222b456df04826a5b53f160e76efe8'
-  const limit = 5
-
-  const {
-    data: { data: posts },
-  } = await axios.get(`https://api.instagram.com/v1/users/self/media/recent?access_token=5414559434.a5e0d4b.f4222b456df04826a5b53f160e76efe8&count=5`)
-
-  return {
-    statusCode: 200,
-    headers: {
-      'content-type': 'application/json',
-    },
-    body: JSON.stringify(
-      posts.map(i => ({
-        id: i.id,
-        link: i.link,
-        images: i.images,
-        videos: i.videos,
-        caption: i.caption.text,
-      })),
-    ),
-  }
-}
+exports.handler = async () => ({
+  statusCode: 200,
+  body: `NODE_ENV: ${process.env.NODE_ENV}, SECOND_VAR: ${process.env.SECOND_VAR}`,
+});
