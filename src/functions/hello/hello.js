6b6ef75039e36cc686bbdd01ec62195db8fec4be
pugongyingh <pgyhh@sina.cn>
2019-10-23 15:48:42 +0800

    Update hello.js

--- src/functions/hello/hello.js
+++ src/functions/hello/hello.js
@@ -2,5 +2,6 @@
 
 exports.handler = async () => ({
   statusCode: 200,
-  body: `NODE_ENV: ${process.env.NODE_ENV}, SECOND_VAR: ${process.env.SECOND_VAR}`,
+  body: `NODE_ENV: 333, SECOND_VAR: 666`,
+   //body: `NODE_ENV: ${process.env.NODE_ENV}, SECOND_VAR: ${process.env.SECOND_VAR}`,
 });
