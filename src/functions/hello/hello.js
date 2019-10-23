515a91f99944fbc75c4d9bdbc6794a9a35fe2125
pugongyingh <pgyhh@sina.cn>
2019-10-23 15:58:58 +0800

    Update hello.js

--- src/functions/hello/hello.js
+++ src/functions/hello/hello.js
@@ -1,8 +1,10 @@
 const axios = require('axios');
-//const cheerio = require('cheerio');
+ 
 
-exports.handler = function (event, context, callback) {
 
+ 
+
+exports.handler = async () => ({
     const url = 'https://m.zhouyi.cc/bazi/sm/bazi.php';
     const query = {
         url,
@@ -16,39 +18,10 @@ exports.handler = function (event, context, callback) {
 
     result = await axios.request(query);
     
-     send(result.data);
     
-
-
-    // get data from url
-    // then send it
-
-
-    // send response
-    // note data is passed in response body
-    // note also headers 
-    const send = data => {
-        callback(null, {
-            statusCode: 200,
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
-            },
-            body: data
-        });
-    }
-
-    // can restrict HTTP method here
-    //if (event.httpMethod === 'GET') {        
-    
-    //}
-
-} // exports.handler
-
-//exports.handler = async () => ({
- // statusCode: 200,
-//  body: `NODE_ENV: 333, SECOND_VAR: 666`,
+  statusCode: 200,
+  body: result.data,
    //body: `NODE_ENV: ${process.env.NODE_ENV}, SECOND_VAR: ${process.env.SECOND_VAR}`,
-//});
+});
 
 
