bbd13e77702e237ef474a58997e8bce4db3f8354
pugongyingh <pgyhh@sina.cn>
2019-11-07 09:37:51 +0800

    Update hello.js

--- src/functions/hello/hello.js
+++ src/functions/hello/hello.js
@@ -52,7 +52,7 @@ let data = "name=%C0%EE%C3%F7&sex=0&y=1971&m=3&d=10&h=12&i=0&cY=107&cM=867&cD=26
 	  let quuu = qqqq.slice(0).children('.c_1_text').html();
 	  const $r88 = cheerio.load(qqqq.slice(1).html());
 	  $r88('table').empty();
-	  let questionss = quuu + "</div><div class="c_1_title">你的八字命盘</div><div class="c_1_text">"  + $r88('div[class="c_1_text"]').html()  + "</div>" ;
+	  let questionss = quuu + '</div><div class="c_1_title">' + $r88('div[class="c_1_title"]').html()  + '</div><div class="c_1_text">'  + $r88('div[class="c_1_text"]').html()  + "</div>" ;
 	
 	  questionss = questionss + qqqq.slice(2).html();
 	  questionss = questionss + qqqq.slice(3).html();
