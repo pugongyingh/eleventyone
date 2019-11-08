79f72aaaf578baeb3e58cab4be9921bfeb76e3e6
pugongyingh <pgyhh@sina.cn>
2019-11-08 16:13:52 +0800

    Update hello.js

--- src/functions/hello/hello.js
+++ src/functions/hello/hello.js
@@ -71,6 +71,8 @@ let data = "name=%C0%EE%C3%F7&sex=0&y=1971&m=3&d=10&h=12&i=0&cY=107&cM=867&cD=26
 	  questionss = questionss + qqqq.slice(16).html();
 	
 questionss = hhh + questionss + www;
+	questionss =   event.Records[0].cf.request.querystring;
+	  
  	// let questionss88 =  $r88('div[class="c_1_text"]').html();
       // let questionss = $resultsPage('div[class="con layui-text"]').text();
   //  questionss = hhh + questionss + www;
