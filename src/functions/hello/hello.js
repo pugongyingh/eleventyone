b432fdd9edbb34474fefd28fcd2822e142eb7e8b
pugongyingh <pgyhh@sina.cn>
2019-11-09 17:30:36 +0800

    Update hello.js

--- src/functions/hello/hello.js
+++ src/functions/hello/hello.js
@@ -53,7 +53,7 @@ var html = iconv.decode(response.data, 'gb2312'); //return unicode string from G
 	  let qqqq = $resultsPage('div[class="suanming_s"]').children('div[class="suanming_c_1"]'); 
 	  qqqq = cheerio.load(qqqq.html());
 	  let quuu = qqqq.slice(0).children('.c_1_text');
-	  const $r88 = qqqq.slice(1).html()
+	  const $r88 = qqqq.slice(1).html();
 	  $r88('table').empty();
 	  let questionss = quuu + '</div><div class="c_1_title">' + $r88('div[class="c_1_title"]').html()  + '</div><div class="c_1_text">'  + $r88('div[class="c_1_text"]').html()  + "</div>" ;
 	
