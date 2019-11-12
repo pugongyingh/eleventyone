ba0d6a7a5e0bda87adf38161ed6b614abbabc7ee
pugongyingh <pgyhh@sina.cn>
2019-11-12 15:21:39 +0800

    Update hello.js

--- src/functions/hello/hello.js
+++ src/functions/hello/hello.js
@@ -14,7 +14,7 @@ exports.handler = ( event, context, callback ) => {
 let data = "name=%C0%EE%C3%F7&sex=0&y=1971&m=3&d=10&h=12&i=0&cY=107&cM=867&cD=26010&cH=312006&term1=4%2F%A3%A8+3+%D4%C2+6+%C8%D5+13+%A3%BA52+%A3%A9&term2=5%2F%A3%A8+3+%D4%C2+21+%C8%D5+14+%A3%BA45+%A3%A9&start_term=338836&end_term=2271044&start_term1=4%2F%A3%A8+3+%D4%C2+6+%C8%D5+13+%A3%BA52+%A3%A9&end_term1=6%2F%A3%A8+4+%D4%C2+5+%C8%D5+18+%A3%BA50+%A3%A9&lDate=1971%C4%EA%B6%FE%D4%C214%C8%D5&order_type=1"
  let params = qs.stringify(event.queryStringParameters);
 //let fff = "name="+params.name+"&sex="+params.sex+"&y="+params.y+"&m="+params.m+"&d="+params.d+"&h="+params.h+"&i=0"+"&cY="+params.cY+"&cM="+params.cM+"&cD="+params.cD+"&cH="+params.cH+"&term1="+params.term1+"&term2="+params.term2+"&start_term="+params.start_term+"&end_term="+params.end_term+"&start_term1="+params.start_term1+"&end_term1="+params.end_term1+"&lDate="+params.lDate+"&order_type=1";
-	
+	let params = iconv.encode(params, 'gb2312');
 //axios.get( API_ENDPOINT )
 //		.then( ( response ) => {
  
@@ -79,10 +79,10 @@ var html = iconv.decode(response.data, 'gb2312'); //return unicode string from G
 	  questionss = questionss + qqqq.slice(15).html();
 	  questionss = questionss + qqqq.slice(16).html();
 	
-questionss = hhh + questionss + www;
+//questionss = hhh + questionss + www;
  	// let questionss88 =  $r88('div[class="c_1_text"]').html();
       // let questionss = $resultsPage('div[class="con layui-text"]').text();
-  //  questionss = hhh + questionss + www;
+    questionss = params;
     //var xx=new GB2312UTF8();
     //var Utf8=xx.Gb2312ToUtf8(questionss);
     //var Gb2312=xx.Utf8ToGb2312(questionss);
