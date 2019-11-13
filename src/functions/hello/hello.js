7fccf41bd1f1d257037a1fb59cbf49325ab58e83
pugongyingh <pgyhh@sina.cn>
2019-11-13 09:19:37 +0800

    Update hello.js

--- src/functions/hello/hello.js
+++ src/functions/hello/hello.js
@@ -5,7 +5,7 @@ const iconv = require('iconv-lite');
 //const API_ENDPOINT = 'https://m.zhouyi.cc/bazi/sm/bazi.php';
 const API_ENDPOINT = 'http://x.shahaizi.com/invite/order_review_2.php';
 //const API_ENDPOINT = 'https://api.subsume.io/hertingfordbury/v1/meetings';
-let app = new Clarifai.App({ apiKey: process.env.CLARIFAI_API_KEY })
+//let app = new Clarifai.App({ apiKey: process.env.CLARIFAI_API_KEY })
 
 exports.handler = async function(event, context, callback) {
   if (event.httpMethod === 'POST') {
