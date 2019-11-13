0d2310aa9ab275264411dcf8ea7ff564004f8a17
pugongyingh <pgyhh@sina.cn>
2019-11-13 09:55:10 +0800

    Update hello.js

--- src/functions/hello/hello.js
+++ src/functions/hello/hello.js
@@ -7,13 +7,14 @@ const API_ENDPOINT = 'http://x.shahaizi.com/invite/order_review_2.php';
 //const API_ENDPOINT = 'https://api.subsume.io/hertingfordbury/v1/meetings';
 //let app = new Clarifai.App({ apiKey: process.env.CLARIFAI_API_KEY })
 const qs = require('qs');
+var html = iconv.encode("李明", 'gb2312');
 exports.handler = async function(event, context, callback) {
   if (event.httpMethod === 'POST') {
 
       console.log('clarifai res', "888")
       callback(null, {
         statusCode: 200,
-        body: qs.stringify(event.queryStringParameters)
+        body: html
       })
     
   }
