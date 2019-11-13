0c73bf2781a9593a82c195c4ab17b630aba760d9
pugongyingh <pgyhh@sina.cn>
2019-11-13 10:40:52 +0800

    Update hello.js

--- src/functions/hello/hello.js
+++ src/functions/hello/hello.js
@@ -8,10 +8,8 @@ const API_ENDPOINT = 'http://x.shahaizi.com/invite/order_review_2.php';
 //let app = new Clarifai.App({ apiKey: process.env.CLARIFAI_API_KEY })
 const qs = require('qs');
 //const html = iconv.encode("李明", 'gb2312');
-exports.handler = async function(event, context, callback) {
-  if (event.httpMethod === 'POST') {
-   var str = str || '';
-    str = iconv.encode("李明", 'gbk').toString('Hex');
+
+function urlEcode(str) {
    var  arr = [];
     while(str.length) {
       var substr = str.substring(0, 2);
@@ -21,11 +19,19 @@ exports.handler = async function(event, context, callback) {
 
     str = arr.join('%');
     str = '%' + str;
-  
+
+  return str;
+}
+
+exports.handler = async function(event, context, callback) {
+  if (event.httpMethod === 'POST') {
+   var str = str || '';
+    str = iconv.encode("李明", 'gbk').toString('Hex');
+
       console.log('clarifai res', "888")
       callback(null, {
         statusCode: 200,
-        body: str.toString()
+        body: urlEcode(str)
       })
     
   }
