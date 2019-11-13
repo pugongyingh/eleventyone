c59405f84e81b69cbce3c28ea3568826007113e9
pugongyingh <pgyhh@sina.cn>
2019-11-13 16:23:23 +0800

    Update hello.js

--- src/functions/hello/hello.js
+++ src/functions/hello/hello.js
@@ -222,24 +222,94 @@ function urlEcode(str) {
 	}
 
 
-exports.handler = async function(event, context, callback) {
+
+
+exports.handler = ( event, context, callback ) => {
   let params = event.queryStringParameters;
 
 
 let uuu =  checkForm(params.n,params.s,params.y,params.m,params.d,params.h);
 
-	if (event.httpMethod === 'GET') {
 
+  axios.post(API_ENDPOINT,uuu,{ responseType: "arraybuffer" }).then(function(response){
+      
+var html = iconv.decode(response.data, 'gb2312'); //return unicode string from GBK encoded bytes
 
-   
-      callback(null, {
-        statusCode: 200,
-        body: uuu
-      })
+//var html = iconv.encode(str, 'gb2312');//return GBK encoded bytes from unicode string    
     
-  }
-}
+    // 也可以通过 params 对象传递参数
+//axios.get('/user', {params: {ID: 12345}}).then(function (response) {
+    
+    
+		//const	body = iconv.decode(response.data,'gb2312');
+    	//const	body = iconv.decode(response.data,'utf-8').toString();
+       //const $resultsPage = cheerio.load(response.data);
+	const $resultsPage = cheerio.load(html, { decodeEntities: false });
 
+ 
+ let hhh = '<html><head><link href="https://sharp-almeida-02bb5b.netlify.com/ttt.css" rel="stylesheet" type="text/css"></head><body><div class="app"><div class="subs_2"><div class="c_1_title"><strong>&#x751F;&#x8FB0;&#x516B;&#x5B57;&#x8BE6;&#x6279;</strong></div><div class="c_1_text">';
+   let www = '</div></div></body></html>'; 
+ //   var fruits = [];
+
+//$resultsPage('div[class="suanming_s"]').children().each(function(i, elem) {
+//  fruits[i] = $(this).html();
+//});
+
+//fruits.join(', ');
+//=> Apple, Orange, Pear
+    
+    //let questionss = $resultsPage('div[class="suanming_s"]').find('suanming_c_1').html();
+     // let questionss = $resultsPage('div[class="suanming_s"]').children('div[class="suanming_c_1"]').removeClass("c_2_title").html();
+	//  let qqqq = $resultsPage('div[class="suanming_s"]').children('div[class="suanming_c_1"]'); 
+	//  let quuu = qqqq.slice(0).children('.c_1_text').html();
+	 let   qqqqq = cheerio.load($resultsPage.html());
+	
+	  let qqqq = qqqqq('div[class="suanming_s"]').children('div[class="suanming_c_1"]'); 
+	  let quuu = qqqq.slice(0).children('.c_1_text').html();
+	  const $r88 = cheerio.load(qqqq.slice(1).html());
+	  $r88('table').empty();
+	  let questionss = quuu + '</div><div class="c_1_title">' + $r88('div[class="c_1_title"]').html()  + '</div><div class="c_1_text">'  + $r88('div[class="c_1_text"]').html()  + "</div>" ;
+	
+	  questionss = questionss + qqqq.slice(2).html();
+	  questionss = questionss + qqqq.slice(3).html();
+	  questionss = questionss + qqqq.slice(4).html();
+	  questionss = questionss + qqqq.slice(5).html();
+	  questionss = questionss + qqqq.slice(6).html();
+	  questionss = questionss + qqqq.slice(7).html();
+	  questionss = questionss + qqqq.slice(8).html();
+	  questionss = questionss + qqqq.slice(9).html();
+	  questionss = questionss + qqqq.slice(10).html();
+	  questionss = questionss + qqqq.slice(11).html();
+	  questionss = questionss + qqqq.slice(12).html();
+	  questionss = questionss + qqqq.slice(13).html();
+	  questionss = questionss + qqqq.slice(14).html();
+	  questionss = questionss + qqqq.slice(15).html();
+	  questionss = questionss + qqqq.slice(16).html();
+	
+questionss = hhh + questionss + www;
+ 	// let questionss88 =  $r88('div[class="c_1_text"]').html();
+      // let questionss = $resultsPage('div[class="con layui-text"]').text();
+   // questionss = params + dataa;
+    //var xx=new GB2312UTF8();
+    //var Utf8=xx.Gb2312ToUtf8(questionss);
+    //var Gb2312=xx.Utf8ToGb2312(questionss);
+    //var encoder = new TextEncoder('gbk');
+
+   // var Utf8=encoder.encode(questionss);
+			callback( null, {
+				headers: {
+         'content-type': 'text/html; charset=gb2312',
+        // 'content-type': 'text/html; charset=utf-8',
+				},
+				statusCode: 200,
+     body: questionss,
+      
+			} );
+		} )
+		.catch( ( error ) => {
+			callback( error );
+		} );
+};
 
 
 
